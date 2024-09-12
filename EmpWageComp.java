f4c7509226fbad3f62f0377146a207ec660b5c9f
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-12 09:13:08 +0530

    The Daily wage computation

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,20 +1,29 @@
 class EmpWageComp{
-    public static void main(String [] args)
-    {
-        
+    
 
-
+public class Main {
+    public static void main(String[] args) {
+        int wagePerHour = 20;
+        int fullDay = 8;
         System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+        System.out.println("");
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if(empCheck == 0)
         {
-            System.out.println("Employee is Present");
+            System.out.print("Employee is Present");
+            System.out.println("");
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.println("");
         }
         else
         {
-            System.out.println("Employee is Absent");
+            System.out.print("Employee is Absent");
+            System.out.println("");
+            System.out.println("The daily wage of Employee is : 0 " );
+            System.out.println("");
         }
 
     }
+}
 }
