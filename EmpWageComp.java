c52e17349f633c973ab5ae151cb5e6afcf6e266a
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-12 09:05:31 +0530

    The attendance check

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,6 +1,20 @@
 class EmpWageComp{
     public static void main(String [] args)
     {
-        System.out.println("Welcome to employee wage computation");
+        
+
+
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
