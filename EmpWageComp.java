2665538211973f3c62b6beea1757fd6e36d17d28
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-12 09:20:25 +0530

    Adding Part time employee details and wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,19 +1,28 @@
 class EmpWageComp{
     
 
-public class Main {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         int wagePerHour = 20;
         int fullDay = 8;
+        int halfDay = 4;
         System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
         System.out.println("");
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if(empCheck == 0)
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if( empCheck == 0 )
         {
             System.out.print("Employee is Present");
             System.out.println("");
             System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.println("");
+        }
+
+        else if ( empCheck == 1 )
+
+        {
+            System.out.print("Employee is Present but working part time ");
+            System.out.println("");
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
             System.out.println("");
         }
         else
@@ -25,5 +34,4 @@ public class Main {
         }
 
     }
-}
 }
