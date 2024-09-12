b5f10e93624cf846632f1c873fbc638c3da6205d
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-12 09:28:41 +0530

    Using the switch case for employee wage computation

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -9,28 +9,28 @@ class EmpWageComp{
         System.out.println("");
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if( empCheck == 0 )
-        {
-            System.out.print("Employee is Present");
-            System.out.println("");
-            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
-            System.out.println("");
-        }
+        switch ( empCheck ) {
+            case 0:
+                System.out.print("Employee is Present");
+                System.out.println("");
+                System.out.print("The daily wage of Employee is : " + (wagePerHour * fullDay));
+                System.out.println("");
+                break;
 
-        else if ( empCheck == 1 )
+            case 1:
+                System.out.print("Employee is Present but working part time ");
+                System.out.println("");
+                System.out.print("The daily wage of Employee is : " + (wagePerHour * halfDay));
+                System.out.println("");
+                break;
 
-        {
-            System.out.print("Employee is Present but working part time ");
-            System.out.println("");
-            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
-            System.out.println("");
-        }
-        else
-        {
-            System.out.print("Employee is Absent");
-            System.out.println("");
-            System.out.println("The daily wage of Employee is : 0 " );
-            System.out.println("");
+            case 2:
+                System.out.print("Employee is Absent");
+                System.out.println("");
+                System.out.println("The daily wage of Employee is : 0 ");
+                System.out.println("");
+                break;
+
         }
 
     }
