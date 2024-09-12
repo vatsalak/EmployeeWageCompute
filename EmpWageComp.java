1d628b082a064b3ad583ac43fc190b0bfef078d9
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-12 08:58:12 +0530

    first commit

---
+++ EmpWageComp.java
@@ -0,0 +1,6 @@
+class EmpWageComp{
+    public static void main(String [] args)
+    {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# EmployeeWageCompute
